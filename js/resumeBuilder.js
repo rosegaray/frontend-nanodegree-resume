b0928a2b3628b9389ad1504ede34a5001cac8abf
Rose <rgaray@umd.edu>
2015-05-24 21:07:04 -0400

    name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,7 @@
-$("#main").append([Rose Garay]);
+$("#main").append("Rose Garay");
+var formattedName = "Rose Garay";
+var formattedRole = "Software Developper"
+HTMLheaderName.replace(%data%, formattedName);
+HTMLheaderRole.replace(%data%, formattedRole);
+$("#Header").append(formattedName);
+$("#header").prepend(formattedRole);
